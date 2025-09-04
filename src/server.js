42b5745d1aaacea6a32cb7e8233b129228378fa9
Xarch <rakhaarkhana98@gmail.com>
2025-09-04 22:59:23 +0700

    setup

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const dotenv = require("dotenv");
+const connectDb = require("./config/mongoDb");
+const app = require("./index");
+
+dotenv.config();
+connectDb();
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+  console.log("http://localhost:5000");
+});
